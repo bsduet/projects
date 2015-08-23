c771d3497494d6475972ef40591c048491b2ec6a
Bisnu <bsduet@gmail.com>
2015-08-23 15:00:42 +0600

    upload project

---
+++ OCATest/src/OCATest.java
@@ -0,0 +1,34 @@
+public class OCATest extends student{
+	//static int z = 4;
+
+	public static void main(String arg[]) {
+		int[] array = { 1, 2, 3, 4, 5 };
+		System.arraycopy(array, 2, array, 1, 2);
+		System.out.print(array[1]);
+		System.out.print(array[4]);
+		
+		StudentTest asd = new StudentTest();
+		asd.jodi("bisnu");
+		
+	}
+
+	String as = "";
+
+	@Override
+	int getName() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// int z1=6;
+
+	// public static int getLetter();
+	// a[i][j]=
+	// public static void Main(String[] args) {
+	//
+	// System.out.println(args.length);
+	// //System.out.println(args[1]);
+	// // System.out.println(args[2]);
+	// }
+
+}
---
+++ OCATest/src/StudentTest.java
@@ -0,0 +1,14 @@
+
+public class StudentTest {
+
+	public void jodi(String a){
+	int[][] arr  = {{0,1,2,3}, {2,3,4,5,}};
+	
+	System.out.print(arr[1].getClass().isArray());
+	}
+	
+}
+abstract class student{
+	abstract int  getName();
+	//abstract int getID(){return 5;}
+}
